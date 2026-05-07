4ba74ababfd4f7750cd58513bb13fb033769d2d1
heryko <122610954+heryko@users.noreply.github.com>
2026-05-07 17:37:39 +0200

    tak tak

---
+++ utils.py
@@ -0,0 +1,21 @@
+"""Exemplary calculator helper functions."""
+
+
+def add(a: int, b: int) -> int:
+    """Return the sum of two integers."""
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
+    """Return the difference of two integers."""
+    return a - b
+
+
+def multiply(a: int, b: int) -> int:
+    """Return the product of two integers."""
+    return a * b
+
+
+def divide(a: int, b: int) -> float:
+    """Return the quotient of two integers as a float."""
+    return a / b
